9eb6c593c436bb33a7aff2246fa5f1c2d3134fde
Timur <TiM76rus@yandex.ru>
2022-01-31 21:25:10 +0300

    2nd commit

--- js/script.js
+++ js/script.js
@@ -2,14 +2,25 @@
 
 const URL = 'https://5ebbb8e5f2cfeb001697d05c.mockapi.io/users';
 
-const checkForIE = () => {
-    const ua = navigator.userAgent;
-    let isIE = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
-    console.log(isIE);
-
+const fetchData = url => {
+    return new Promise(((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = () => {
+            console.log(xhr.response)
+        };
+        xhr.responseType = 'json';
+        xhr.send();
+    }))
 }
-checkForIE();
 
+let data = fetchData(URL);
+
+setTimeout(() => console.log(data), 7000);
+
+
+
+/*
 async function loadUser(url) {
     try {
         const response = await fetch(url);
@@ -20,25 +31,31 @@ async function loadUser(url) {
         console.error(err);
     }
 }
+*/
 
-const drawTable = users => {
-    const tableData = document.querySelector('#table-body');
 
-    for (let i = 0; i < users.length; i++) {
-        const userRow = `<tr>
-                            <td>${users[i].username}</td>
-                            <td>${users[i].email}</td>
-                            <td>${users[i].registration_date}</td>
-                            <td>${users[i].rating}</td>
-                            <td class="delete">X</td>
-                         </tr>
-        `;
-        tableData.innerHTML += userRow;
-    }
-}
+// const drawTable = users => {
+//     const tableData = document.querySelector('#table-body');
+//
+//     for (let i = 0; i < users.length; i++) {
+//         const userRow = `<tr>
+//                             <td>${users[i].username}</td>
+//                             <td>${users[i].email}</td>
+//                             <td>${users[i].registration_date}</td>
+//                             <td>${users[i].rating}</td>
+//                             <td class="delete">X</td>
+//                          </tr>
+//         `;
+//         tableData.innerHTML += userRow;
+//     }
+// }
 
 const searchUser = (name, email) => {
     const input = document.querySelector('#form-input');
+}
+
+const showModal = () => {
+    document.querySelector('')
 }
 
 loadUser(URL);
